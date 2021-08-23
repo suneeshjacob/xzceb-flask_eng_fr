d2a9571b3c7fadbb23f96598f50a64c012c39f89
Jacob <suneeshjacob@gmail.com>
2021-08-23 02:16:37 +0000

    Making to track files

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from translator import *
+
+class testing(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(englishToFrench('Hello'),'Bonjour')
+    def test_bonjour(self):
+        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
+    def test_eng_null(self):
+        self.assertEqual(englishToFrench(''),'')
+    def test_fr_null(self):
+        self.assertEqual(frenchToEnglish(''),'')
+
+if __name__=='__main__':
+    unittest.main()
+
